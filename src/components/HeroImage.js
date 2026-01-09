0840affdc165e0835d3cd33363b5266c9430b67a
Bhushan Date <bhushan.date.94@gmail.com>
2026-01-09 09:35:10 +0530

    Updated Hero Image for both

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,3 +1,19 @@
+// import MainImg from "../images/Hero-Photo-Alt.jpg";
+
+// function HeroImage() {
+//   return (
+//     <div className="pt-8 px-4 md:pt-10 md:px-8 lg:pt-16 lg:px-16">
+//       <img
+//         src={MainImg}
+//         alt="Hero"
+//         className="w-full h-[120vh] md:h-[120vh] lg:h-[120vh] object-cover rounded-lg"
+//       />
+//     </div>
+//   );
+// }
+
+// export default HeroImage;
+
 import MainImg from "../images/Hero-Photo-Alt.jpg";
 
 function HeroImage() {
@@ -6,7 +22,7 @@ function HeroImage() {
       <img
         src={MainImg}
         alt="Hero"
-        className="w-full h-[120vh] md:h-[120vh] lg:h-[120vh] object-cover rounded-lg"
+        className="w-full h-auto object-contain rounded-lg"
       />
     </div>
   );
